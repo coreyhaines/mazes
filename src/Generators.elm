f16c9baed8af84a2359b6e7e42b163d83de8d8d5
Corey Haines <coreyhaines@gmail.com>
2016-12-31 16:22:59 -0600

    Initial pull out of a generators module

---
+++ src/Generators.elm
@@ -0,0 +1,123 @@
+module Generators exposing (Algorithm(..), getAlgorithm, Room, Side(..))
+
+import Random
+import List.Extra as List
+
+
+type Algorithm
+    = PlainGrid
+    | BinaryTree
+    | Sidewinder
+
+
+type alias Room =
+    { x : Int
+    , y : Int
+    , walls : Side
+    }
+
+
+type Side
+    = All
+    | Right
+    | Top
+
+
+type alias MazeGenerator =
+    { seed : Random.Seed, width : Int, height : Int } -> List Room -> ( List Room, Random.Seed )
+
+
+plainGridAlgorithm : MazeGenerator
+plainGridAlgorithm { seed } rooms =
+    ( rooms, seed )
+
+
+binaryTreeAlgorithm : MazeGenerator
+binaryTreeAlgorithm { seed, width } rooms =
+    let
+        pickASide which =
+            if which then
+                Right
+            else
+                Top
+
+        nextWalls fromSeed room =
+            if (room.y == 0) && (room.x == width - 1) then
+                ( All, fromSeed )
+            else if room.y == 0 then
+                ( Top, fromSeed )
+            else if room.x == width - 1 then
+                ( Right, fromSeed )
+            else
+                Random.step (Random.map pickASide Random.bool) fromSeed
+
+        figureOutSide room ( updatedRooms, currentSeed ) =
+            let
+                ( walls, nextSeed ) =
+                    nextWalls currentSeed room
+            in
+                ( { room | walls = walls } :: updatedRooms
+                , nextSeed
+                )
+    in
+        List.foldl
+            figureOutSide
+            ( [], seed )
+            rooms
+
+
+sidewinderAlgorithm : MazeGenerator
+sidewinderAlgorithm { seed, width, height } rooms =
+    let
+        getRow index rooms =
+            List.filter (\{ x, y, walls } -> y == index) rooms
+
+        sameRoom room1 room2 =
+            (room1.x == room2.x && room1.y == room2.y)
+
+        replaceRoom room ( updatedRooms, currentSeed ) =
+            ( List.replaceIf (sameRoom room) room updatedRooms
+            , currentSeed
+            )
+
+        replaceRooms index rooms ( roomsToReplace, seed ) =
+            List.foldl
+                replaceRoom
+                ( rooms, seed )
+                roomsToReplace
+
+        updateTopRow width index ( rooms, seed ) =
+            ( List.map
+                (\room ->
+                    if room.x < width - 1 then
+                        { room | walls = Top }
+                    else
+                        { room | walls = All }
+                )
+                rooms
+            , seed
+            )
+
+        processRow width height index ( rooms, seed ) =
+            ( rooms, seed )
+
+        updateRow processor index ( rooms, seed ) =
+            ( getRow index rooms, seed )
+                |> processor index
+                |> replaceRooms index rooms
+    in
+        updateRow (updateTopRow width) 0 ( rooms, seed )
+            |> updateRow (processRow width height) 1
+
+
+getAlgorithm : Algorithm -> MazeGenerator
+getAlgorithm algorithm =
+    case algorithm of
+        PlainGrid ->
+            plainGridAlgorithm
+
+        BinaryTree ->
+            binaryTreeAlgorithm
+
+        Sidewinder ->
+            sidewinderAlgorithm
--- src/Main.elm
+++ src/Main.elm
@@ -1,5 +1,6 @@
 module Main exposing (..)
 
+import Generators exposing (Algorithm(..), Room, Side)
 import Editable exposing (Editable)
 import Html exposing (Html, text, div, input, label, button, span)
 import Html.Attributes exposing (style, type_, value)
@@ -15,29 +16,6 @@ type alias Model =
     , rooms : List Room
     , algorithm : Algorithm
     }
-
-
-type alias Room =
-    { x : Int
-    , y : Int
-    , walls : Side
-    }
-
-
-type Algorithm
-    = PlainGrid
-    | BinaryTree
-    | Sidewinder
-
-
-type Side
-    = All
-    | Right
-    | Top
-
-
-type alias Sides =
-    List Side
 
 
 type Dimension
@@ -93,10 +71,6 @@ init =
         )
 
 
-type alias MazeGenerator =
-    { seed : Random.Seed, width : Int, height : Int } -> List Room -> ( List Room, Random.Seed )
-
-
 coordinates : Int -> Int -> List ( Int, Int )
 coordinates width height =
     List.lift2 (,) (List.range 0 (width - 1)) (List.range 0 (height - 1))
@@ -105,110 +79,14 @@ coordinates width height =
 generateRooms : Model -> List Room
 generateRooms model =
     coordinates (mazeWidth model) (mazeHeight model)
-        |> List.map (\( x, y ) -> { x = x, y = y, walls = All })
-
-
-plainGridAlgorithm : MazeGenerator
-plainGridAlgorithm { seed } rooms =
-    ( rooms, seed )
-
-
-binaryTreeAlgorithm : MazeGenerator
-binaryTreeAlgorithm { seed, width } rooms =
-    let
-        pickASide which =
-            if which then
-                Right
-            else
-                Top
-
-        nextWalls fromSeed room =
-            if (room.y == 0) && (room.x == width - 1) then
-                ( All, fromSeed )
-            else if room.y == 0 then
-                ( Top, fromSeed )
-            else if room.x == width - 1 then
-                ( Right, fromSeed )
-            else
-                Random.step (Random.map pickASide Random.bool) fromSeed
-
-        figureOutSide room ( updatedRooms, currentSeed ) =
-            let
-                ( walls, nextSeed ) =
-                    nextWalls currentSeed room
-            in
-                ( { room | walls = walls } :: updatedRooms
-                , nextSeed
-                )
-    in
-        List.foldl
-            figureOutSide
-            ( [], seed )
-            rooms
-
-
-sidewinderAlgorithm : MazeGenerator
-sidewinderAlgorithm { seed, width, height } rooms =
-    let
-        getRow index rooms =
-            List.filter (\{ x, y, walls } -> y == index) rooms
-
-        sameRoom room1 room2 =
-            (room1.x == room2.x && room1.y == room2.y)
-
-        replaceRoom room ( updatedRooms, currentSeed ) =
-            ( List.replaceIf (sameRoom room) room updatedRooms
-            , currentSeed
-            )
-
-        replaceRooms index rooms ( roomsToReplace, seed ) =
-            List.foldl
-                replaceRoom
-                ( rooms, seed )
-                roomsToReplace
-
-        updateTopRow width index ( rooms, seed ) =
-            ( List.map
-                (\room ->
-                    if room.x < width - 1 then
-                        { room | walls = Top }
-                    else
-                        { room | walls = All }
-                )
-                rooms
-            , seed
-            )
-
-        processRow width height index ( rooms, seed ) =
-            ( rooms, seed )
-
-        updateRow processor index ( rooms, seed ) =
-            ( getRow index rooms, seed )
-                |> processor index
-                |> replaceRooms index rooms
-    in
-        updateRow (updateTopRow width) 0 ( rooms, seed )
-            |> updateRow (processRow width height) 1
-
-
-getAlgorithm : Algorithm -> MazeGenerator
-getAlgorithm algorithm =
-    case algorithm of
-        PlainGrid ->
-            plainGridAlgorithm
-
-        BinaryTree ->
-            binaryTreeAlgorithm
-
-        Sidewinder ->
-            sidewinderAlgorithm
+        |> List.map (\( x, y ) -> { x = x, y = y, walls = Generators.All })
 
 
 generateMaze : Algorithm -> Model -> Model
 generateMaze algorithm model =
     let
         mazeGenerator =
-            getAlgorithm algorithm
+            Generators.getAlgorithm algorithm
 
         ( rooms, finalSeed ) =
             generateRooms model
@@ -284,13 +162,13 @@ roomView { x, y, walls } =
 
         ( topBorder, rightBorder ) =
             case walls of
-                Right ->
+                Generators.Right ->
                     ( "0px", "1px solid black" )
 
-                Top ->
+                Generators.Top ->
                     ( "1px solid black", "0px" )
 
-                All ->
+                Generators.All ->
                     ( "1px solid black", "1px solid black" )
     in
         div
