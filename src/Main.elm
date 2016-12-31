66d97e2c89f0609c1bbcb3ab4eb842c6ec3fc0d6
Corey Haines <coreyhaines@gmail.com>
2016-12-30 19:37:56 -0600

    ignore git stuff

--- src/Main.elm
+++ src/Main.elm
@@ -1,11 +1,13 @@
 module Main exposing (..)
 
-import Html exposing (Html, text)
+import Html exposing (Html, text, div)
+import Html.Attributes exposing (style)
 import Random
 
 
 type alias Model =
-    { sides : Sides
+    { size : Int
+    , sides : Sides
     }
 
 
@@ -22,6 +24,10 @@ type Msg
     = SetSides Sides
 
 
+scale =
+    3
+
+
 pickASide : Bool -> Side
 pickASide which =
     if which then
@@ -32,9 +38,13 @@ pickASide which =
 
 init : ( Model, Cmd Msg )
 init =
-    ( { sides = [] }
-    , Random.generate SetSides (Random.list 5 <| Random.map pickASide Random.bool)
-    )
+    let
+        size =
+            10
+    in
+        ( { size = size, sides = [] }
+        , Random.generate SetSides (Random.list (size * size) <| Random.map pickASide Random.bool)
+        )
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
@@ -51,9 +61,23 @@ subscriptions model =
     Sub.none
 
 
+borderView : Int -> Html Msg
+borderView size =
+    let
+        pixels =
+            (toString (size * size * scale)) ++ "px"
+    in
+        div [ style [ ( "position", "relative" ), ( "border", "1px solid black" ), ( "width", pixels ), ( "height", pixels ) ] ]
+            []
+
+
 view : Model -> Html Msg
 view model =
-    text <| toString model
+    div []
+        [ borderView model.size
+        , Html.hr [] []
+        , text <| toString model
+        ]
 
 
 main : Program Never Model Msg
